46178e5aee4c57bb945a82f78454e3b32f6e2800
Abhishek Kalyan Adupa <abhishek.adupa@columbia.edu>
2021-05-10 19:09:35 -0400

    sample test

--- src/test/java/com/abhishek/tradingsystem/TradingsystemApplicationTests.java
+++ src/test/java/com/abhishek/tradingsystem/TradingsystemApplicationTests.java
@@ -1,12 +1,23 @@
 package com.abhishek.tradingsystem;
 
+import com.abhishek.tradingsystem.model.TradingService;
+import com.abhishek.tradingsystem.model.enums.Operation;
+import com.abhishek.tradingsystem.model.enums.OrderType;
+import com.abhishek.tradingsystem.model.enums.Symbol;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 class TradingsystemApplicationTests {
     @Test
     void setUp() {
-
+        TradingService tradingService = new TradingService();
+        Assertions.assertTrue(tradingService.addOrder(new BigDecimal(10), Symbol.AAPL, 2, LocalDateTime.now(), Operation.SELL, OrderType.MARKET));
+        System.out.println(tradingService.listOrders(Operation.SELL));
+        Assertions.assertTrue(tradingService.addOrder(new BigDecimal(20), Symbol.AAPL, 2, LocalDateTime.now(), Operation.SELL, OrderType.MARKET));
+        System.out.println(tradingService.listOrders(Operation.SELL));
     }
 
 }
